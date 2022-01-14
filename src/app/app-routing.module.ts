ab4653d9df3ad123cd648def6d061631be03ceac
Rohan Thakare <rohaanthakare@gmail.com>
2022-01-14 19:48:16 +0530

    Forms added Login, Register, Forgot Password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './core/components/forgot-password/forgot-password.component';
 import { LoginComponent } from './core/components/login/login.component';
+import { RegisterComponent } from './core/components/register/register.component';
 
 const routes: Routes = [{
   path: "",
   component: LoginComponent
+}, {
+  path: "forgot-password",
+  component: ForgotPasswordComponent
+}, {
+  path: "register",
+  component: RegisterComponent
 }, {
   path: "home",
   loadChildren: () => import("./core/core.module").then(m => m.CoreModule)
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { CardModule } from "primeng/card";
+import { ImageModule } from "primeng/image";
+import { InputTextModule } from "primeng/inputtext";
+import { PasswordModule } from "primeng/password";
+import { ButtonModule } from "primeng/button";
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { MainContainerComponent } from './components/main-container/main-container.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [{
   path: "",
@@ -20,10 +27,13 @@ const routes: Routes = [{
     DashboardComponent,
     HeaderComponent,
     NavigationComponent,
-    MainContainerComponent
+    MainContainerComponent,
+    ForgotPasswordComponent,
+    RegisterComponent
   ],
   imports: [
-    CommonModule, RouterModule.forChild(routes)
+    CommonModule, RouterModule.forChild(routes), InputTextModule, ButtonModule, CardModule,
+    ImageModule, PasswordModule
   ]
 })
 export class CoreModule { }
